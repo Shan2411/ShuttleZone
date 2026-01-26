3265818c0e8a9aae96806c522438f73b33052b8a
rodge <rodgepatrick2005@gmail.com>
2026-01-26 22:31:06 +0800

    POS (rental and membership checkout)

---
+++ ShuttleZone/UC_Pos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShuttleZone
+{
+    public partial class UC_Pos : UserControl
+    {
+        public UC_Pos()
+        {
+            InitializeComponent();
+        }
+
+        private void UC_Pos_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
