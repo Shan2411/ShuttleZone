bd3dd859da9219ea270c25039b2fb0b8a37f0fb7
rodge <rodgepatrick2005@gmail.com>
2026-02-08 23:54:41 +0800

    added sidebar buttons

--- ShuttleZone/Kiosk.Designer.cs
+++ ShuttleZone/Kiosk.Designer.cs
@@ -31,9 +31,10 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Kiosk));
             this.tlpRoot = new System.Windows.Forms.TableLayoutPanel();
             this.tlpSidebar = new System.Windows.Forms.TableLayoutPanel();
+            this.btnCourtRental = new Guna.UI2.WinForms.Guna2Button();
             this.btnEquipment = new Guna.UI2.WinForms.Guna2Button();
             this.btnMembership = new Guna.UI2.WinForms.Guna2Button();
-            this.btnCourtRental = new Guna.UI2.WinForms.Guna2Button();
+            this.pnlMainContent = new Guna.UI2.WinForms.Guna2Panel();
             this.tlpRoot.SuspendLayout();
             this.tlpSidebar.SuspendLayout();
             this.SuspendLayout();
@@ -44,6 +45,7 @@
             this.tlpRoot.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 75F));
             this.tlpRoot.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tlpRoot.Controls.Add(this.tlpSidebar, 0, 0);
+            this.tlpRoot.Controls.Add(this.pnlMainContent, 1, 0);
             this.tlpRoot.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tlpRoot.Location = new System.Drawing.Point(0, 0);
             this.tlpRoot.Name = "tlpRoot";
@@ -71,6 +73,26 @@
             this.tlpSidebar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tlpSidebar.Size = new System.Drawing.Size(69, 743);
             this.tlpSidebar.TabIndex = 0;
+            // 
+            // btnCourtRental
+            // 
+            this.btnCourtRental.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnCourtRental.AutoRoundedCorners = true;
+            this.btnCourtRental.BorderRadius = 26;
+            this.btnCourtRental.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnCourtRental.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnCourtRental.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnCourtRental.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnCourtRental.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(35)))), ((int)(((byte)(191)))));
+            this.btnCourtRental.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnCourtRental.ForeColor = System.Drawing.Color.White;
+            this.btnCourtRental.Image = ((System.Drawing.Image)(resources.GetObject("btnCourtRental.Image")));
+            this.btnCourtRental.ImageSize = new System.Drawing.Size(63, 54);
+            this.btnCourtRental.Location = new System.Drawing.Point(3, 23);
+            this.btnCourtRental.Name = "btnCourtRental";
+            this.btnCourtRental.Size = new System.Drawing.Size(63, 54);
+            this.btnCourtRental.TabIndex = 0;
+            this.btnCourtRental.Click += new System.EventHandler(this.guna2Button1_Click);
             // 
             // btnEquipment
             // 
@@ -106,30 +128,19 @@
             this.btnMembership.Size = new System.Drawing.Size(63, 54);
             this.btnMembership.TabIndex = 2;
             // 
-            // btnCourtRental
+            // pnlMainContent
             // 
-            this.btnCourtRental.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnCourtRental.AutoRoundedCorners = true;
-            this.btnCourtRental.BorderRadius = 26;
-            this.btnCourtRental.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
-            this.btnCourtRental.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
-            this.btnCourtRental.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
-            this.btnCourtRental.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
-            this.btnCourtRental.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(35)))), ((int)(((byte)(191)))));
-            this.btnCourtRental.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.btnCourtRental.ForeColor = System.Drawing.Color.White;
-            this.btnCourtRental.Image = ((System.Drawing.Image)(resources.GetObject("btnCourtRental.Image")));
-            this.btnCourtRental.ImageSize = new System.Drawing.Size(63, 54);
-            this.btnCourtRental.Location = new System.Drawing.Point(3, 23);
-            this.btnCourtRental.Name = "btnCourtRental";
-            this.btnCourtRental.Size = new System.Drawing.Size(63, 54);
-            this.btnCourtRental.TabIndex = 0;
-            this.btnCourtRental.Click += new System.EventHandler(this.guna2Button1_Click);
+            this.pnlMainContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMainContent.Location = new System.Drawing.Point(78, 3);
+            this.pnlMainContent.Name = "pnlMainContent";
+            this.pnlMainContent.Size = new System.Drawing.Size(383, 743);
+            this.pnlMainContent.TabIndex = 1;
             // 
             // Kiosk
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(464, 749);
             this.ControlBox = false;
             this.Controls.Add(this.tlpRoot);
@@ -151,5 +162,6 @@
         private Guna.UI2.WinForms.Guna2Button btnEquipment;
         private Guna.UI2.WinForms.Guna2Button btnMembership;
         private Guna.UI2.WinForms.Guna2Button btnCourtRental;
+        private Guna.UI2.WinForms.Guna2Panel pnlMainContent;
     }
 }
