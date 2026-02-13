feddc7edba975ab5189f8ea1e049a920237596c7
rodge <rodgepatrick2005@gmail.com>
2026-02-13 20:08:05 +0800

    Update POS button to load UC_Pos user control dynamically

--- ShuttleZone/Form1.cs
+++ ShuttleZone/Form1.cs
@@ -122,6 +122,10 @@ using System.Web.Security;
             {
                 HighlightButton(POSBtn);
                 DynamicContentPanel.Controls.Clear();
+                UC_Pos ucPos = new UC_Pos();
+                ucPos.Dock = DockStyle.Fill;
+
+                DynamicContentPanel.Controls.Add(ucPos);
             }
 
             private void RentalHistoryBtn_Click(object sender, EventArgs e)
