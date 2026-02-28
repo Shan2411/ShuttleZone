4f3eae7655fdbaaeeb702fc1053e72751e3b4498
Shan2411 <shandavid2411@gmail.com>
2026-02-28 20:47:24 +0800

    slightly better db frontdesk

--- ShuttleZone/Dashboard1/CourtCard.Designer.cs
+++ ShuttleZone/Dashboard1/CourtCard.Designer.cs
@@ -48,15 +48,15 @@
             this.guna2Panel1.BorderColor = System.Drawing.Color.DarkOliveGreen;
             this.guna2Panel1.BorderRadius = 7;
             this.guna2Panel1.BorderThickness = 2;
-            this.guna2Panel1.Controls.Add(this.tableLayoutPanel1);
             this.guna2Panel1.Controls.Add(this.guna2Button2);
+            this.guna2Panel1.Controls.Add(this.tableLayoutPanel1);
             this.guna2Panel1.Controls.Add(this.guna2CirclePictureBox1);
             this.guna2Panel1.Controls.Add(this.label1);
             this.guna2Panel1.Controls.Add(this.guna2Panel3);
             this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.guna2Panel1.Location = new System.Drawing.Point(0, 0);
             this.guna2Panel1.Name = "guna2Panel1";
-            this.guna2Panel1.Size = new System.Drawing.Size(285, 341);
+            this.guna2Panel1.Size = new System.Drawing.Size(310, 378);
             this.guna2Panel1.TabIndex = 1;
             // 
             // guna2Button2
@@ -72,9 +72,9 @@
             this.guna2Button2.FillColor = System.Drawing.Color.MediumSeaGreen;
             this.guna2Button2.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2Button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.guna2Button2.Location = new System.Drawing.Point(39, 221);
+            this.guna2Button2.Location = new System.Drawing.Point(36, 253);
             this.guna2Button2.Name = "guna2Button2";
-            this.guna2Button2.Size = new System.Drawing.Size(212, 29);
+            this.guna2Button2.Size = new System.Drawing.Size(237, 29);
             this.guna2Button2.TabIndex = 8;
             this.guna2Button2.Text = "available";
             // 
@@ -91,7 +91,7 @@
             this.guna2CirclePictureBox1.Location = new System.Drawing.Point(250, 6);
             this.guna2CirclePictureBox1.Name = "guna2CirclePictureBox1";
             this.guna2CirclePictureBox1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
-            this.guna2CirclePictureBox1.Size = new System.Drawing.Size(27, 32);
+            this.guna2CirclePictureBox1.Size = new System.Drawing.Size(52, 69);
             this.guna2CirclePictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.guna2CirclePictureBox1.TabIndex = 6;
             this.guna2CirclePictureBox1.TabStop = false;
@@ -118,7 +118,7 @@
             this.guna2Panel3.Controls.Add(this.guna2Panel4);
             this.guna2Panel3.Location = new System.Drawing.Point(91, 61);
             this.guna2Panel3.Name = "guna2Panel3";
-            this.guna2Panel3.Size = new System.Drawing.Size(100, 151);
+            this.guna2Panel3.Size = new System.Drawing.Size(125, 188);
             this.guna2Panel3.TabIndex = 3;
             // 
             // guna2Panel4
@@ -132,7 +132,7 @@
             this.guna2Panel4.BorderThickness = 1;
             this.guna2Panel4.Location = new System.Drawing.Point(0, 53);
             this.guna2Panel4.Name = "guna2Panel4";
-            this.guna2Panel4.Size = new System.Drawing.Size(100, 139);
+            this.guna2Panel4.Size = new System.Drawing.Size(125, 176);
             this.guna2Panel4.TabIndex = 0;
             // 
             // tableLayoutPanel1
@@ -143,12 +143,12 @@
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.Controls.Add(this.guna2HtmlLabel1, 0, 0);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 250);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 278);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 78.40909F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 21.59091F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(282, 88);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 61.85567F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 38.14433F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(307, 97);
             this.tableLayoutPanel1.TabIndex = 9;
             // 
             // guna2HtmlLabel1
@@ -157,9 +157,9 @@
             this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
             this.guna2HtmlLabel1.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.guna2HtmlLabel1.Font = new System.Drawing.Font("Consolas", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.guna2HtmlLabel1.Location = new System.Drawing.Point(3, 15);
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(3, 6);
             this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
-            this.guna2HtmlLabel1.Size = new System.Drawing.Size(276, 51);
+            this.guna2HtmlLabel1.Size = new System.Drawing.Size(301, 51);
             this.guna2HtmlLabel1.TabIndex = 10;
             this.guna2HtmlLabel1.Text = "Ready For Booking";
             this.guna2HtmlLabel1.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
@@ -171,7 +171,7 @@
             this.Controls.Add(this.guna2Panel1);
             this.Margin = new System.Windows.Forms.Padding(50, 30, 50, 30);
             this.Name = "CourtCard";
-            this.Size = new System.Drawing.Size(285, 341);
+            this.Size = new System.Drawing.Size(310, 378);
             this.guna2Panel1.ResumeLayout(false);
             this.guna2Panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox1)).EndInit();
