ac729e587206566953aeac9d9ad0d9ee65727752
wionquintela <wionquintela06@gmail.com>
2026-03-18 18:46:30 +0800

    protected overrided form1 - no stutter

--- ShuttleZone/Form1.cs
+++ ShuttleZone/Form1.cs
@@ -1,182 +1,158 @@
-﻿    using ShuttleZone.Dashboard1;
-    using ShuttleZone.Maintenance_Logs;
-    using ShuttleZone.sidebars;
+﻿using ShuttleZone.Dashboard1;
+using ShuttleZone.Maintenance_Logs;
+using ShuttleZone.sidebars;
 using ShuttleZone.SystemSettings;
 using ShuttleZone.topbar;
-    using ShuttleZone.UserManagement;
-    using System;
-    using System.Drawing;
-    using System.Web.Security;
-    using System.Windows.Forms;
-
+using ShuttleZone.UserManagement;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace ShuttleZone
+{
+    public partial class Form1 : Form
     {
-        public partial class Form1 : Form
+        // 🔥 Store all views here (REUSABLE)
+        private Dictionary<Type, UserControl> _views = new Dictionary<Type, UserControl>();
+
+        public Form1()
         {
-            public Form1()
-            {
-                InitializeComponent();
+            InitializeComponent();
 
-                this.Load += Form1_Load;
+            // 🔥 Enable double buffering (fix flicker)
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer
+                        | ControlStyles.AllPaintingInWmPaint
+                        | ControlStyles.UserPaint, true);
 
-            //AdminSidebar adminSidebarUC = new AdminSidebar();
-            //SidebarDynamicPanel.Controls.Clear();
-            //SidebarDynamicPanel.Controls.Add(adminSidebarUC);
+            this.UpdateStyles();
+
+            this.Load += Form1_Load;
+
             var date = DateTime.Now;
             DateLbl.Text = date.ToString("dddd, MMMM dd, yyyy");
         }
 
-            private void Form1_Load(object sender, EventArgs e)
+        // 🔥 HARDCORE flicker fix (Windows-level)
+        protected override CreateParams CreateParams
+        {
+            get
             {
-                // Set current date to label
-                //DateLbl.Text = DateTime.Now.ToString("dddd, MMMM dd, yyyy");
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x02000000; // WS_EX_COMPOSITED
+                return cp;
+            }
+        }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
 
+        // 🔥 GENERIC VIEW LOADER (CORE SYSTEM)
+        private void LoadView<T>() where T : UserControl, new()
+        {
+            DynamicContentPanel.SuspendLayout();
+
+            if (!_views.ContainsKey(typeof(T)))
+            {
+                var view = new T();
+                view.Dock = DockStyle.Fill;
+                _views[typeof(T)] = view;
             }
 
-            private void HistoryBtn_Click(object sender, EventArgs e)
-            {
             DynamicContentPanel.Controls.Clear();
+            DynamicContentPanel.Controls.Add(_views[typeof(T)]);
+
+            DynamicContentPanel.ResumeLayout();
         }
+
+        // ================= BUTTONS =================
 
         private void UsersBtn_Click(object sender, EventArgs e)
         {
-            DynamicContentPanel.Controls.Clear();
-            UC_UserManagement ucUserManagement = new UC_UserManagement();
-            ucUserManagement.Dock = DockStyle.Fill;
-            DynamicContentPanel.Controls.Add(ucUserManagement);
+            LoadView<UC_UserManagement>();
+        }
+
+        private void MembershipBtn_Click(object sender, EventArgs e)
+        {
+            LoadView<UC_Membership>();
+        }
+
+        private void POSBtn_Click(object sender, EventArgs e)
+        {
+            LoadView<UC_Pos>();
+        }
+
+        private void InventoryBtn_Click(object sender, EventArgs e)
+        {
+            LoadView<Equipment_and_Inventory.Equipment>();
         }
 
         private void FacilityBtn_Click(object sender, EventArgs e)
-            {
-            DynamicContentPanel.Controls.Clear();
-            MaintenanceWindow maintenanceWindowUC = new MaintenanceWindow();
-            maintenanceWindowUC.Dock = DockStyle.Fill;
-            DynamicContentPanel.Controls.Add(maintenanceWindowUC);
-        }
-
-        //DASHBOARD BUTTON CLICKS
-        private void AdminDashboardBtn_Click(object sender, EventArgs e)
         {
-              
-            DynamicContentPanel.Controls.Clear();
-            
-            Dashboard1.AdminDashboard ucDashboard = new Dashboard1.AdminDashboard();
-            DynamicContentPanel.Controls.Add(ucDashboard);
-            ucDashboard.Dock = DockStyle.Fill;
-        }
-        private void FrontDeskDashboardBtn_Click(object sender, EventArgs e)
-        {
-
-            DynamicContentPanel.Controls.Clear();
-
-            Dashboard1.FrontDeskDashboard ucDashboard = new Dashboard1.FrontDeskDashboard();
-            DynamicContentPanel.Controls.Add(ucDashboard);
-            ucDashboard.Dock = DockStyle.Fill;
-        }
-        private void ManagerDashboardBtn_Click(object sender, EventArgs e)
-        {
-
-            DynamicContentPanel.Controls.Clear();  
-            Dashboard1.ManagerDashboard ucDashboard = new Dashboard1.ManagerDashboard();
-            DynamicContentPanel.Controls.Add(ucDashboard);
-            ucDashboard.Dock = DockStyle.Fill;
-        }
-
-        //
-
-
-        private void MembershipBtn_Click(object sender, EventArgs e)
-            {
-           
-                //logic for changing pages on dynamic panel
-                DynamicContentPanel.Controls.Clear();
-                UC_Membership ucMembership = new UC_Membership();
-                ucMembership.Dock = DockStyle.Fill;
-
-                DynamicContentPanel.Controls.Add(ucMembership);
-            }
-
-            private void ExitBtn_Click(object sender, EventArgs e)
-            {
-                Application.Exit();
-            }
-
-            private void POSBtn_Click(object sender, EventArgs e)
-            {
-         
-                DynamicContentPanel.Controls.Clear();
-                UC_Pos ucPos = new UC_Pos();
-                ucPos.Dock = DockStyle.Fill;
-
-                DynamicContentPanel.Controls.Add(ucPos);
-            }
-
-            private void InventoryBtn_Click(object sender, EventArgs e)
-            {
-                DynamicContentPanel.Controls.Clear();
-                Equipment_and_Inventory.Equipment equipmentWindowUC = new Equipment_and_Inventory.Equipment();
-                DynamicContentPanel.Controls.Add(equipmentWindowUC);
-                equipmentWindowUC.Dock = DockStyle.Fill;
-        }
-
-            private void SettingsBtn_Click(object sender, EventArgs e)
-            {
-                DynamicContentPanel.Controls.Clear();
-                UC_SystemsSettings ucSettings = new UC_SystemsSettings();
-                ucSettings.Dock = DockStyle.Fill;
-
-                DynamicContentPanel.Controls.Add(ucSettings);
+            LoadView<MaintenanceWindow>();
         }
 
         private void KioskBtn_Click(object sender, EventArgs e)
-            {
-                DynamicContentPanel.Controls.Clear();
-                UC_Kiosk kioskUC = new UC_Kiosk();
-                kioskUC.Dock = DockStyle.Fill;
-                DynamicContentPanel.Controls.Add(kioskUC);
-            }
+        {
+            LoadView<UC_Kiosk>();
+        }
 
-            public void MaintenanceLogBtn_Click(object sender, EventArgs e)
-            {
-              
-                DynamicContentPanel.Controls.Clear();
+        private void ReportsBtn_Click(object sender, EventArgs e)
+        {
+            LoadView<Reports.Reports_Dashboard>();
+        }
 
-                Maintenance_Logs.MaintenanceWindow maintenanceWindowUC = new Maintenance_Logs.MaintenanceWindow();
-                DynamicContentPanel.Controls.Add(maintenanceWindowUC);
-                maintenanceWindowUC.Dock = DockStyle.Fill;
-            }
+        private void SettingsBtn_Click(object sender, EventArgs e)
+        {
+            LoadView<UC_SystemsSettings>();
+        }
 
-            private void ReportsBtn_Click(object sender, EventArgs e)
-            {
-            
-                DynamicContentPanel.Controls.Clear();
+        private void HistoryBtn_Click(object sender, EventArgs e)
+        {
+            DynamicContentPanel.Controls.Clear();
+        }
 
-                Reports.Reports_Dashboard reportsDashboardUC = new Reports.Reports_Dashboard();
-                DynamicContentPanel.Controls.Add(reportsDashboardUC);
-                reportsDashboardUC.Dock = DockStyle.Fill;
-            }
+        // ================= DASHBOARDS =================
+
+        private void AdminDashboardBtn_Click(object sender, EventArgs e)
+        {
+            LoadView<AdminDashboard>();
+        }
+
+        private void FrontDeskDashboardBtn_Click(object sender, EventArgs e)
+        {
+            LoadView<FrontDeskDashboard>();
+        }
+
+        private void ManagerDashboardBtn_Click(object sender, EventArgs e)
+        {
+            LoadView<ManagerDashboard>();
+        }
+
+        // ================= ROLE SWITCHING =================
 
         private void ManagerBtn_Click(object sender, EventArgs e)
         {
             DynamicTopbarPanel.Controls.Clear();
             MFTopbar mfTopbarUC = new MFTopbar();
-            DynamicTopbarPanel.Controls.Add(mfTopbarUC);
             mfTopbarUC.Dock = DockStyle.Fill;
+            DynamicTopbarPanel.Controls.Add(mfTopbarUC);
 
             SidebarDynamicPanel.Controls.Clear();
             DynamicContentPanel.Controls.Clear();
+
             ManagerSidebar managerSidebarUC = new ManagerSidebar();
             managerSidebarUC.Dock = DockStyle.Fill;
 
-            
+            // 🔥 Hook events
             managerSidebarUC.ManagerDashboardBtnClicked += ManagerDashboardBtn_Click;
             managerSidebarUC.MembershipBtnClicked += MembershipBtn_Click;
             managerSidebarUC.InventoryBtnClicked += InventoryBtn_Click;
             managerSidebarUC.FacilityBtnClicked += FacilityBtn_Click;
             managerSidebarUC.UsersBtnClicked += UsersBtn_Click;
             managerSidebarUC.KioskBtnClicked += KioskBtn_Click;
+
             SidebarDynamicPanel.Controls.Add(managerSidebarUC);
 
             mfTopbarUC.AdminBtnClicked += AdminBtn_Click;
@@ -188,17 +164,19 @@ namespace ShuttleZone
         {
             DynamicTopbarPanel.Controls.Clear();
             AdminTopbar adminTopbarUC = new AdminTopbar();
-            DynamicTopbarPanel.Controls.Add(adminTopbarUC);
             adminTopbarUC.Dock = DockStyle.Fill;
+            DynamicTopbarPanel.Controls.Add(adminTopbarUC);
 
             SidebarDynamicPanel.Controls.Clear();
             DynamicContentPanel.Controls.Clear();
+
             AdminSidebar adminSidebarUC = new AdminSidebar();
             adminSidebarUC.Dock = DockStyle.Fill;
 
             adminSidebarUC.AdminDashboardBtnClicked += AdminDashboardBtn_Click;
             adminSidebarUC.ReportsBtnClicked += ReportsBtn_Click;
             adminSidebarUC.UsersBtnClicked += UsersBtn_Click;
+
             SidebarDynamicPanel.Controls.Add(adminSidebarUC);
 
             adminTopbarUC.AdminBtnClicked += AdminBtn_Click;
@@ -211,11 +189,12 @@ namespace ShuttleZone
         {
             DynamicTopbarPanel.Controls.Clear();
             MFTopbar mfTopbarUC = new MFTopbar();
-            DynamicTopbarPanel.Controls.Add(mfTopbarUC);
             mfTopbarUC.Dock = DockStyle.Fill;
+            DynamicTopbarPanel.Controls.Add(mfTopbarUC);
 
             SidebarDynamicPanel.Controls.Clear();
             DynamicContentPanel.Controls.Clear();
+
             FrontDeskSidebar frontDeskSidebarUC = new FrontDeskSidebar();
             frontDeskSidebarUC.Dock = DockStyle.Fill;
 
@@ -223,6 +202,7 @@ namespace ShuttleZone
             frontDeskSidebarUC.POSBtnClicked += POSBtn_Click;
             frontDeskSidebarUC.MembershipBtnClicked += MembershipBtn_Click;
             frontDeskSidebarUC.HistoryBtnClicked += HistoryBtn_Click;
+
             SidebarDynamicPanel.Controls.Add(frontDeskSidebarUC);
 
             mfTopbarUC.AdminBtnClicked += AdminBtn_Click;
@@ -230,19 +210,11 @@ namespace ShuttleZone
             mfTopbarUC.FrontDeskBtnClicked += FrontDeskBtn_Click;
         }
 
-        private void DynamicContentPanel_Paint(object sender, PaintEventArgs e)
-        {
-                
-        }
+        // ================= SYSTEM =================
 
-        private void Topbar_Paint(object sender, PaintEventArgs e)
+        private void ExitBtn_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void DateLbl_Click(object sender, EventArgs e)
-        {
-           
+            Application.Exit();
         }
     }
-    }
+}
